46d002765a07d6c99d5b9470307bdae7d5ec1a4e
Chris Loew <cloew123@gmail.com>
2015-07-21 09:20:39 -0600

    Added Boilerplate Package code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='kao_listdict',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['kao_listdict'],
+     )
